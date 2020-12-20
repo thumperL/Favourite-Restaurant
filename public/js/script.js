2e5c49a3f2a04c319408ad4780b444639f0b2951
Thumper <shaunwhl1991@gmail.com>
2020-12-20 18:52:50 +1100

    Add CRUD buttons per restaurant
    Add CRUD buttons per restaurant
    Remove old JSON
    Add tooltip for the restaurant CRUD buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,9 @@
-if (document.querySelectorAll('h1.restaurant-show-title').length > 0) {
-  document.querySelector('.search-bar').remove();
-}
+$(() => {
+  // Enable Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Hide/Show search bar
+  if (document.querySelectorAll('h1.restaurant-show-title').length > 0) {
+    document.querySelector('.search-bar').remove();
+  }
+});
