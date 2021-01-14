3991429ced602c71d78db46439be7de0f9e65f0c
Thumper <shaunwhl1991@gmail.com>
2021-01-14 19:25:01 +1100

    feat: add login/register page, and add basic register handling in route
    feat: add login/register page, and add basic register handling in route

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,13 @@ const router = express.Router();
 const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
 const search = require('./modules/search');
+const users = require('./modules/users');
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home);
 router.use('/restaurants/', restaurants);
 router.use('/search/', search);
+router.use('/users', users);
 
 // 匯出路由器
 module.exports = router;
---
+++ routes/modules/users.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+
+const User = require('../../models/user');
+
+// Login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+router.post('/login', (req, res, next) => {
+
+});
+
+// Logout
+router.get('/logout', (req, res) => {
+
+});
+
+// Register
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const {
+    name, email, password, confirmPassword,
+  } = req.body;
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      // 如果已經註冊：退回原本畫面
+      console.log('User already exists.');
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password,
+      })
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err));
+    }
+  });
+});
+module.exports = router;
