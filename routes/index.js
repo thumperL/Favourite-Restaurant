ae7316fe3b1d6d674f8ce612b3b4ca5922267af8
Thumper <shaunwhl1991@gmail.com>
2021-01-15 09:06:13 +1100

    feat: add user authentication in restaurant, home, and search routers
    feat: add user authentication in restaurant, home, and search routers

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@
 const express = require('express');
 
 const router = express.Router();
-// 準備引入路由模組
-// 引入 home 模組程式碼
+// 引入路由模組
 const home = require('./modules/home');
 const restaurants = require('./modules/restaurants');
 const search = require('./modules/search');
 const users = require('./modules/users');
+const { authenticator } = require('../middleware/auth');
 
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home);
-router.use('/restaurants/', restaurants);
-router.use('/search/', search);
+// Request 導向各個模組
+router.use('/restaurants/', authenticator, restaurants);
+router.use('/search/', authenticator, search);
 router.use('/users', users);
+router.use('/', authenticator, home);
 
 // 匯出路由器
 module.exports = router;
